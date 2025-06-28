41a1e7dd833ac0045ed6ba323130f64343d05a68
phanphuockhai2554@gmail.com <phanphuockhai2554@gmail.com>
2025-06-28 07:57:25 +0000

    fix the related-syntax bugs

--- module4/ny_taxi_rides/models/staging/stg_green_tripdata.sql
+++ module4/ny_taxi_rides/models/staging/stg_green_tripdata.sql
@@ -39,7 +39,7 @@ select
     cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
-    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
+    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}, 0) as payment_type,
     {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
 where rn = 1
